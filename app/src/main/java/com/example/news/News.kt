894cb052c22948f26dd8081f74eb384c90f1630e
Dary311 <92427357+Dary311@users.noreply.github.com>
2023-12-12 17:44:09 +0500

    first commit

---
+++ app/src/main/java/com/example/news/News.kt
@@ -0,0 +1,13 @@
+package com.example.news
+
+
+//Определение модели данных News
+data class News (
+
+    val title :String,
+    val author :String,
+    val url :String,
+    val imageUrl:String,
+    val time:String,
+
+    )
---
+++ app/src/main/java/com/example/news/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.example.news
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatDelegate
+
+//отображение экрана приветствия (сплеш-экрана) при запуске приложения.
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // to avoid dark theme
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+
+
+}
+}
